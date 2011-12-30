8d3a736ea5d6234ac9d9c2be54bdfe70c94ac0a6
hide2 <hideto.bj@gmail.com>
2011-12-31 02:37:20 +0800

    gitolite.conf

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 require 'sinatra/base'
 
-
 class App < Sinatra::Base
+  GITOLITE_ADMIN_HOME = "../gitolite-admin"
+  GITOLITE_ADMIN_KEYDIR = "#{GITOLITE_ADMIN_HOME}/keydir"
+  GITOLITE_ADMIN_CONF = "#{GITOLITE_ADMIN_HOME}/conf"
+  GITOLITE_ADMIN_CONF_FILE = "#{GITOLITE_ADMIN_CONF}/gitolite.conf"
 
   get '/' do
     erb :index
